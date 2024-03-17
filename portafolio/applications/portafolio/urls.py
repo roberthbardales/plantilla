3ae3c4a8cc31b066d103473e3a04abed9b647b3d
user.name <roberthbardales@hotmail.com>
2024-03-17 16:30:04 -0500

    configuracion basica + crud de templates

--- portafolio/applications/portafolio/urls.py
+++ portafolio/applications/portafolio/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = "portafolio_app"
 urlpatterns = [
     path('',views.home,name='home'),
+    path('add',views.add,name='add'),
+    path('list',views.list,name='list'),
+    # path('add2',views.add2,name='add2'),
+    path('update',views.update,name='update'),
+    path('delete',views.delete,name='delete'),
 
 ]
--- portafolio/applications/portafolio/views.py
+++ portafolio/applications/portafolio/views.py
@@ -5,3 +5,18 @@ from . import views
 def home(request):
     return render(request,'index.html')
 
+def add(request):
+    return render(request,'add.html')
+
+def list(request):
+    return render(request,'list.html')
+
+# def add2(request):
+#     return render(request,'add2.html')
+
+def update(request):
+    return render(request,'update.html')
+
+def delete(request):
+    return render(request,'delete.html')
+
